54d01ca87cfc59ba501dfd4e5140549ee0f6a5c3
vindeshvari <vindeshvarimishra786@gmail.com>
2026-01-29 23:48:40 -0800

    Fix currUser undefined in EJS

--- app.js
+++ app.js
@@ -75,9 +75,9 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
+  res.locals.currUser = req.user || null;
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
-  res.locals.currUser = req.user;
   next();
 });
 
